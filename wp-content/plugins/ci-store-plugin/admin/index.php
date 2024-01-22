760fd8b90e3b42ebf3a5b8ff852876335ba46283
drivej <drivej@hotmail.com>
2024-01-22 08:31:43 -0500

    x

--- wp-content/plugins/ci-store-plugin/admin/index.php
+++ wp-content/plugins/ci-store-plugin/admin/index.php
@@ -1,3 +1,23 @@
 <?php
 
 include_once __DIR__ . '/product_admin.php';
+
+function import_products_trigger()
+{
+    // Your tasks to be executed on schedule
+    // For example, update some data, send emails, etc.
+    error_log('Scheduled action executed!');
+}
+
+register_activation_hook(__FILE__, 'schedule_import_products');
+
+function schedule_import_products()
+{
+    // Schedule the event to run hourly
+    if (!wp_next_scheduled('import_products_event')) {
+        // wp_schedule_event(time(), 'hourly', 'import_products_event');
+        wp_schedule_event(time(), 'every_minute', 'import_products_event');
+    }
+}
+
+add_action('import_products_event', 'import_products_trigger');
